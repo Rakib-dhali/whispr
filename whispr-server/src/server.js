8d44421f7c2df80745a6434f7481458ef4e740f5
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-05-31 12:15:56 +0600

    feat: initialize express server with production static file serving configuration

--- whispr-server/src/server.js
+++ whispr-server/src/server.js
@@ -1,25 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path"
 
 dotenv.config();
 
 const app = express();
+const _dirname = path.resolve()
 const PORT = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../whispr-client/dist")))
 
-const clientDistPath = path.join(__dirname, '../../whispr-client/dist');
-
-// Serve static files from frontend build
-app.use(express.static(clientDistPath));
-
-// Fallback all other requests to frontend's index.html
-app.get('*all', (req, res) => {
-    res.sendFile(path.join(clientDistPath, 'index.html'));
-});
+    app.get("*", (_, res) => {
+        res.sendFile(path.resolve(_dirname, "../whispr-client", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
