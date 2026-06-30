c33a20d056a00e24e2b9b7aa02c1211134763bb0
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-06-30 14:02:29 +0600

    feat: initialize server project and implement email service with welcome and login notification templates

--- whispr-server/src/server.js
+++ whispr-server/src/server.js
@@ -1,29 +1,32 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import path from "path"
-import { connectDB } from './lib/db.js';
-import authRoutes from './routes/auth.route.js';
+import "dotenv/config";
+import dns from "node:dns";
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
 
-dotenv.config();
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
-const _dirname = path.resolve()
+const _dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes )
+app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(_dirname, "../whispr-client/dist")))
+  app.use(express.static(path.join(_dirname, "../whispr-client/dist")));
 
-    app.get("*splat", (_, res) => {
-        res.sendFile(path.resolve(_dirname, "../whispr-client", "dist", "index.html"))
-    })
+  app.get("*splat", (_, res) => {
+    res.sendFile(
+      path.resolve(_dirname, "../whispr-client", "dist", "index.html"),
+    );
+  });
 }
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
